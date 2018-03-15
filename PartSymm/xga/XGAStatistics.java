1feb474290a12bde94120ab539656ab91f24ae33
Stephen Kozakoff <steve@kozakoff.com>
2018-03-14 23:18:13 -0400

    Fix reporting best individual in XGAStatistics

--- PartSymm/xga/XGAStatistics.java
+++ PartSymm/xga/XGAStatistics.java
@@ -143,9 +143,12 @@ public class XGAStatistics extends Statistics implements SteadyStateStatisticsFo
 
 		// for now we just print the best fitness per subpopulation.
 		Individual[] best_i = new Individual[state.population.subpops.length]; // quiets compiler complaints
+		int best_ind_idx = 0;
+		
 		for (int x = 0; x < state.population.subpops.length; x++)
 		{
 			best_i[x] = state.population.subpops[x].individuals[0];
+			
 			for (int y = 1; y < state.population.subpops[x].individuals.length; y++)
 			{
 				if (state.population.subpops[x].individuals[y] == null)
@@ -153,39 +156,67 @@ public class XGAStatistics extends Statistics implements SteadyStateStatisticsFo
 					if (!warned)
 					{
 						state.output.warnOnce("Null individuals found in subpopulation");
-						warned = true; // we do this rather than relying on warnOnce because it is much faster in a
-										// tight loop
+						warned = true; // we do this rather than relying on warnOnce because it is much faster in a tight loop
 					}
 				}
-				else if (best_i[x] == null || state.population.subpops[x].individuals[y].fitness.betterThan(best_i[x].fitness)) best_i[x] = state.population.subpops[x].individuals[y];
+				else if (best_i[x] == null || state.population.subpops[x].individuals[y].fitness.betterThan(best_i[x].fitness)) 
+				{
+					best_i[x] = state.population.subpops[x].individuals[y];
+					best_ind_idx = y;
+				}
+				
 				if (best_i[x] == null)
 				{
 					if (!warned)
 					{
 						state.output.warnOnce("Null individuals found in subpopulation");
-						warned = true; // we do this rather than relying on warnOnce because it is much faster in a
-										// tight loop
+						warned = true; // we do this rather than relying on warnOnce because it is much faster in a tight loop
 					}
 				}
 			}
 
 			// now test to see if it's the new best_of_run
-			if (best_of_run[x] == null || best_i[x].fitness.betterThan(best_of_run[x].fitness)) best_of_run[x] = (Individual) (best_i[x].clone());
+			if (best_of_run[x] == null || best_i[x].fitness.betterThan(best_of_run[x].fitness))
+			{
+				best_of_run[x] = (Individual) (best_i[x].clone());
+			}
 		}
 
 		// print the best-of-generation individual
-		if (doGeneration) state.output.println("\nGeneration: " + state.generation, statisticslog);
-		if (doGeneration) state.output.println("Best Individual:", statisticslog);
+		if (doGeneration)
+		{
+			state.output.println("\nGeneration: " + state.generation, statisticslog);
+		}
+
+		if (doGeneration)
+		{
+			state.output.println(String.format("Best Individual: %d, Fitness: %f ", best_ind_idx, state.population.subpops[0].individuals[best_ind_idx].fitness.fitness()), statisticslog);
+		}
+
 		for (int x = 0; x < state.population.subpops.length; x++)
 		{
-			if (doGeneration) state.output.println("Subpopulation " + x + ":", statisticslog);
-			if (doGeneration) best_i[x].printIndividualForHumans(state, statisticslog);
-			if (doMessage && !silentPrint) state.output.message("Subpop " + x + " best fitness of generation" + (best_i[x].evaluated ? " " : " (evaluated flag not set): ") + best_i[x].fitness.fitnessToStringForHumans());
+			if (doGeneration)
+			{
+				state.output.println("Subpopulation " + x + ":", statisticslog);
+			}
+
+			if (doGeneration)
+			{
+				best_i[x].printIndividualForHumans(state, statisticslog);
+			}
+
+			if (doMessage && !silentPrint)
+			{
+				state.output.message("Subpop " + x + " best fitness of generation" + (best_i[x].evaluated ? " " : " (evaluated flag not set): ") + best_i[x].fitness.fitnessToStringForHumans());
+			}
 
 			// describe the winner if there is a description
 			if (doGeneration && doPerGenerationDescription)
 			{
-				if (state.evaluator.p_problem instanceof SimpleProblemForm) ((SimpleProblemForm) (state.evaluator.p_problem.clone())).describe(state, best_i[x], x, 0, statisticslog);
+				if (state.evaluator.p_problem instanceof SimpleProblemForm)
+				{
+					((SimpleProblemForm) (state.evaluator.p_problem.clone())).describe(state, best_i[x], x, 0, statisticslog);
+				}
 			}
 		}
 	}
@@ -205,16 +236,36 @@ public class XGAStatistics extends Statistics implements SteadyStateStatisticsFo
 		super.finalStatistics(state, result);
 
 		// for now we just print the best fitness
+		if (doFinal)
+		{
+			state.output.println("\nBest Individual of Run:", statisticslog);
+		}
 
-		if (doFinal) state.output.println("\nBest Individual of Run:", statisticslog);
 		for (int x = 0; x < state.population.subpops.length; x++)
 		{
-			if (doFinal) state.output.println("Subpopulation " + x + ":", statisticslog);
-			if (doFinal) best_of_run[x].printIndividualForHumans(state, statisticslog);
-			if (doMessage && !silentPrint) state.output.message("Subpop " + x + " best fitness of run: " + best_of_run[x].fitness.fitnessToStringForHumans());
+			if (doFinal)
+			{
+				state.output.println("Subpopulation " + x + ":", statisticslog);
+			}
+
+			if (doFinal)
+			{
+				best_of_run[x].printIndividualForHumans(state, statisticslog);
+			}
+
+			if (doMessage && !silentPrint)
+			{
+				state.output.message("Subpop " + x + " best fitness of run: " + best_of_run[x].fitness.fitnessToStringForHumans());
+			}
 
 			// finally describe the winner if there is a description
-			if (doFinal && doDescription) if (state.evaluator.p_problem instanceof SimpleProblemForm) ((SimpleProblemForm) (state.evaluator.p_problem.clone())).describe(state, best_of_run[x], x, 0, statisticslog);
+			if (doFinal && doDescription)
+			{
+				if (state.evaluator.p_problem instanceof SimpleProblemForm)
+				{
+					((SimpleProblemForm) (state.evaluator.p_problem.clone())).describe(state, best_of_run[x], x, 0, statisticslog);
+				}
+			}
 		}
 	}
 }
